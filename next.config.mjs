c9c830e72aa18b1c307e0a08b06a6aceeb31ea74
Matthew <matthewajustice@gmail.com>
2023-05-25 12:50:06 -0400

    fixing images for aws

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['main.d2nl8zqqmqrci8.amplifyapp.com'],
+  },
 };
 
 export default nextConfig;
