54337e05a227df8d829a49d07f485bf24be788af
Matthew <matthewajustice@gmail.com>
2023-06-05 00:54:35 -0400

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['amplifyapp.com'],
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
   },
 };
 
